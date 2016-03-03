0d21c0bb715886fe071d8a8fe0d399a0716b2ef4
David A. Russell <david@aprsworld.com>
2016-03-03 15:05:22 -0600

    Updated strip.sh

--- strip.sh
+++ strip.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 # Image file truncator
 #
+# NOTE:  THIS SCRIPT WILL MODIFY THE ORIGINAL FILE PASSED IN!
 # NOTE:  DO NOT USE THIS ON IMAGES WITH LOGICAL PARTITIONS OR USING A PARTITION
 # TYPE OTHER THAN DOS/MBR!!!
 #
@@ -10,50 +11,52 @@
 #
 # So if you have an image file you dd'd off a flash card and run this
 # script it will truncate it to the size of the image including all
-# partitions up the one specified.  It's really not tested against
-# logical partitions and assumes a DOS partition scheme.
+# partitions up the one specified.
 #
-# This script can also be used to simply remove trailing data from
-# a dd'd disk image that is bigger than all the partitions.  Just
-# specify the last partition and it will truncate to the right size.
+# Example: You have an image you dd'd from a disk and want to make the file
+# only as large as needed - `./strip.sh <imagefile>`
 #
 # Example: You have an image with 2 or more partitions and you only
-# want to keep the first two - ./strip.sh <imagefile> 2
-#
-# It works on the original image so is destructive!
+# want to keep the first two - `./strip.sh <imagefile> 2`
 #
 # Written by "David A. Russell" <david@aprsworld.com>
 strImgFile=$1
 lastpartnum=$2
 
-if [[ -z $1 ]]; then
-echo "Usage: ./strip.sh <Image File> [<Last Partition #>]"
-echo "Read comments in script for more info."
-exit
+if [ -z $1 ]; then
+	echo "Usage: ./strip.sh <Image File> [<Last Partition #>]"
+	echo "Read comments in script for more info."
+	exit 127
 fi
 
-if [[ ! -e $1 ]]; then
-echo "Error : Not an image file, or file doesn't exist"
-exit
+if [ ! -e $1 ]; then
+	echo "Error : Not an image file, or file doesn't exist"
+	exit 127
 fi
 
 echo "This will truncate '$1'.  Press Enter to continue."
-read -s
+read discard
 
-origsize=`stat -c "%s" $1`
+origsize=$(stat -c "%s" $1)
 
 if [ ! -z $2 ]; then
 	echo "Removing all partitions after ${2}..."
-	part1=`/sbin/parted $1 -ms rm $(( $2 + 1))-10`
+	/sbin/parted -ms $1 rm $(( $2 + 1 ))-10
 	if [ $? -ne 0 ]; then
 		echo "Error: Could not update partition table; Aborting."
-		exit 1;
+		exit 1
 	fi
 fi
 
-echo "Truncating file to minimal size to contain all remaining partitions..."
-endresult=`/sbin/parted -ms $1 unit B print free | tail -1 | awk -F: ' { print substr($2,0,length($2)-1) } '`
+endresult=$(/sbin/parted -ms $1 unit B print | tail -n 1 | cut -d : -f 3 | sed s/B$// );
+endresult=$(( $endresult + 1 ))
+echo "Truncating file to $endresult bytes..."
 truncate -s $endresult $1
+if [ $? -ne 0 ]; then
+	echo "ERROR: Could not truncate file!"
+	exit 2
+fi
 
-echo "Original Image Size: $origsize"
-echo "New Image Size: $endresult"
+echo "------------------------------------"
+echo "Original Image Size: $origsize bytes"
+echo "New Image Size: $endresult bytes"
